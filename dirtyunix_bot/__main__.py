1d7e5eb4a3b4ebd01e2a9d326dae2410c5422a26
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-08 00:52:29 -0800

    Add proper logging.

--- dirtyunix_bot/__main__.py
+++ dirtyunix_bot/__main__.py
@@ -1,6 +1,9 @@
 """Main class for bot."""
 
+import logging
+import sys
 import time
+from logging.handlers import RotatingFileHandler
 
 import tweepy
 
@@ -10,32 +13,54 @@ import send
 # Delay between tweets in seconds.
 DELAY = 3600
 
+
+def set_up_logging():
+    """Set up proper logging."""
+    logger = logging.getLogger("root")
+    logger.setLevel(logging.DEBUG)
+
+    # Log everything verbosely to a file.
+    file_handler = RotatingFileHandler(filename="log", maxBytes=1024000000, backupCount=10)
+    verbose_form = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s")
+    file_handler.setFormatter(verbose_form)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    # Provide a stdout handler logging at INFO.
+    stream_handler = logging.StreamHandler(sys.stdout)
+    simple_form = logging.Formatter(fmt="%(message)s")
+    stream_handler.setFormatter(simple_form)
+    stream_handler.setLevel(logging.INFO)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
 if __name__ == "__main__":
     api = send.auth_and_get_api()
 
-    with open("log", "w") as f:
-        while True:
-            garbage = gen.gen()
-            f.write("Tweeting: {}.\n".format(garbage))
-            f.flush()
+    LOG = set_up_logging()
 
-            try:
-                send.send_tweet(api, garbage)
+    while True:
+        garbage = gen.gen()
+        LOG.info("Tweeting: {}.".format(garbage))
 
-            except tweepy.error.TweepError as e:
+        try:
+            send.send_tweet(api, garbage)
 
-                f.write("Tweepy error!\n")
-                f.write("code: {}\n".format(e.code()))
-                f.write("message: {}\n".format(e.message()))
-                f.flush()
+        except tweepy.error.TweepError as e:
 
-                f.write("Unhandled error, breaking.\n")
-                f.flsuh()
-                break
+            if e.message == "Status is a duplicate.":
+                LOG.info("Tweepy duplicate tweet error. Trying to tweet again.")
+                LOG.debug("code: {}".format(e.code()))
+                LOG.debug("message: {}".format(e.message()))
+                continue
 
-                if e.message == "Status is a duplicate.":
-                    continue
+            LOG.error("Unhandled Tweepy error!")
+            LOG.error("code: {}".format(e.code()))
+            LOG.error("message: {}".format(e.message()))
+            LOG.critical("Unhandled error, breaking.")
+            break
 
-            f.write("Sleeping for {} seconds.\n".format(DELAY))
-            f.flush()
-            time.sleep(DELAY)
+        LOG.info("Sleeping for {} seconds.".format(DELAY))
+        time.sleep(DELAY)
