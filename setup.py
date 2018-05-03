884c7cc4a8274714075c7a54f44dc0c0377760f5
Andrew Michaud <dev@mail.andrewmichaud.com>
2018-05-03 13:08:33 -0700

    Pin latest botskeleton

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["dirtyunix_bot = dirtyunix_bot.__main__:main"]
       },
 
-      install_requires=["botskeleton>=2.0.2"],
+      install_requires=["botskeleton>=2.0.4"],
       python_requires=">=3.6",
 
       license="BSD3",
