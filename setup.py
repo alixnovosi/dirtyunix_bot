6ffa2956a643522f225bab5aa5edbedf54900bcb
Andrew Michaud <dev@drew.life>
2019-07-02 21:11:33 -0700

    bot update pinning new botskeleton

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["dirtyunix_bot = dirtyunix_bot.__main__:main"]
       },
 
-      install_requires=["botskeleton>=3.3.3"],
+      install_requires=["botskeleton>=3.3.4"],
       python_requires=">=3.6",
 
       license="BSD3",
